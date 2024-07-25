265f277dfc47bcf0c1943815a1759ae82ac0674e
WillyRumapea <willyrumapea123jr@gmail.com>
2024-07-25 15:44:23 +0700

    project fullstack

---
+++ frontend/js/script.js
@@ -0,0 +1,6 @@
+const addbutton = document.getElementsByClassName("button-add")[0];
+const container = document.getElementById("container");
+
+addbutton.addEventListener("click", () => {
+  window.location.href = "add.html";
+});
